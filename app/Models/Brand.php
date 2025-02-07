dc69a203efbc5a9bc474deaae276c633d356f6f3
abdullah.24 <abdullah@logicsoftbd.com>
2025-02-08 00:43:00 +0600

    added brand

---
+++ app/Models/Brand.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Brand extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+    protected $table = 'lib_brand';
+    protected $fillable = [
+       'name', 'buyer_id','created_by','updated_by'
+    ];
+}
