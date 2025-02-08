27806ba694460d9d11e7cb3771c4d2e52a1220ed
AI Palash <palash@logicsoftbd.com>
2025-02-08 13:32:11 +0600

    Uom Page Development

--- app/Models/LibUom.php
+++ app/Models/LibUom.php
@@ -3,9 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Model;
 
 class LibUom extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+    protected $table = 'lib_uom';
+    protected $fillable = [
+        'uom_name'
+    ];
 }
