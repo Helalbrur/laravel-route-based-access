c56dec36fcc381a836a8a27456def80a7814e17b
AI Palash <palash@logicsoftbd.com>
2025-03-04 22:49:27 +0600

    entry form modify

--- app/Helpers/field_manager_array.php
+++ app/Helpers/field_manager_array.php
@@ -34,12 +34,15 @@ function get_field_manager_arr($index)
 function field_manager_arr()
 {
     $field_manager_arr = array();
+    $field_manager_arr[2][1] = "txt_email";
+    $field_manager_arr[2][2] = "txt_website_name";
     $field_manager_arr[3][1] = "txt_item_group_code";
     $field_manager_arr[8][1] = "cbo_country_name";
     $field_manager_arr[9][1] = "cbo_tag_company_name";
     $field_manager_arr[9][2] = "cbo_tag_party_name";
     $field_manager_arr[10][1] = "cbo_tag_company_name";
     $field_manager_arr[10][2] = "cbo_tag_party_name";
+    $field_manager_arr[11][1] = "cbo_buyer_id";
     return $field_manager_arr;
 }
 
