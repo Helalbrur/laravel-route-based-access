c4ba12bd5a6577187c6e28c4122fda7c1c4cefa8
abdullah.24 <abdullah@logicsoftbd.com>
2025-03-09 17:29:23 +0600

    raname brand related files

--- database/seeders/LibBrandSeeder.php
+++ database/seeders/LibBrandSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
-class BrandSeeder extends Seeder
+class LibBrandSeeder extends Seeder
 {
     /**
      * Run the database seeds.
