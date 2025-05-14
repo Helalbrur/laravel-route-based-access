9d8666445a7f8c1c7e2cb7c7c4aff50519af4bdb
helal <helal@logicsoftbd.com>
2025-05-14 20:42:49 +0600

    plugin theamchange

--- public/skote/assets/js/plugin.js
+++ public/skote/assets/js/plugin.js
@@ -7,33 +7,33 @@
             switch (alreadyVisited) {
                 case "light-mode-switch":
                     document.documentElement.removeAttribute("dir");
-                    if (document.getElementById("bootstrap-style").getAttribute("href") != "/skote/assets/css/bootstrap.min.css")
-                        document.getElementById("bootstrap-style").setAttribute("href", "/skote/assets/css/bootstrap.min.css");
-                    if (document.getElementById("app-style").getAttribute("href") != "/skote/assets/css/app.min.css")
-                        document.getElementById("app-style").setAttribute("href", "/skote/assets/css/app.min.css");
+                    if (document.getElementById("bootstrap-style").getAttribute("href") != window.ASSET_BASE_URL +"/skote/assets/css/bootstrap.min.css")
+                        document.getElementById("bootstrap-style").setAttribute("href", window.ASSET_BASE_URL +"/skote/assets/css/bootstrap.min.css");
+                    if (document.getElementById("app-style").getAttribute("href") != window.ASSET_BASE_URL +"/skote/assets/css/app.min.css")
+                        document.getElementById("app-style").setAttribute("href", window.ASSET_BASE_URL +"/skote/assets/css/app.min.css");
                     document.documentElement.setAttribute("data-bs-theme", "light");
                     break;
                 case "dark-mode-switch":
                     document.documentElement.removeAttribute("dir");
-                    if (document.getElementById("bootstrap-style").getAttribute("href") != "/skote/assets/css/bootstrap.min.css")
-                        document.getElementById("bootstrap-style").setAttribute("href", "/skote/assets/css/bootstrap.min.css");
-                    if (document.getElementById("app-style").getAttribute("href") != "/skote/assets/css/app.min.css")
-                        document.getElementById("app-style").setAttribute("href", "/skote/assets/css/app.min.css");
+                    if (document.getElementById("bootstrap-style").getAttribute("href") != window.ASSET_BASE_URL +"/skote/assets/css/bootstrap.min.css")
+                        document.getElementById("bootstrap-style").setAttribute("href", window.ASSET_BASE_URL +"/skote/assets/css/bootstrap.min.css");
+                    if (document.getElementById("app-style").getAttribute("href") != window.ASSET_BASE_URL +"/skote/assets/css/app.min.css")
+                        document.getElementById("app-style").setAttribute("href", window.ASSET_BASE_URL +"/skote/assets/css/app.min.css");
                     document.documentElement.setAttribute("data-bs-theme", "dark");
                     break;
                 case "rtl-mode-switch":
-                    if (document.getElementById("bootstrap-style").getAttribute("href") != "/skote/assets/css/bootstrap.min.rtl.css")
-                        document.getElementById("bootstrap-style").setAttribute("href", "/skote/assets/css/bootstrap.min.rtl.css");
-                    if (document.getElementById("app-style").getAttribute("href") != "/skote/assets/css/app.min.rtl.css")
-                        document.getElementById("app-style").setAttribute("href", "/skote/assets/css/app.min.rtl.css");
+                    if (document.getElementById("bootstrap-style").getAttribute("href") != window.ASSET_BASE_URL +"/skote/assets/css/bootstrap.min.rtl.css")
+                        document.getElementById("bootstrap-style").setAttribute("href", window.ASSET_BASE_URL +"/skote/assets/css/bootstrap.min.rtl.css");
+                    if (document.getElementById("app-style").getAttribute("href") != window.ASSET_BASE_URL +"/skote/assets/css/app.min.rtl.css")
+                        document.getElementById("app-style").setAttribute("href", window.ASSET_BASE_URL +"/skote/assets/css/app.min.rtl.css");
                     document.documentElement.setAttribute("dir", "rtl");
                     document.documentElement.setAttribute("data-bs-theme", "light");
                     break;
                 case "dark-rtl-mode-switch":
-                    if (document.getElementById("bootstrap-style").getAttribute("href") != "/skote/assets/css/bootstrap.min.rtl.css")
-                        document.getElementById("bootstrap-style").setAttribute("href", "/skote/assets/css/bootstrap.min.rtl.css");
-                    if (document.getElementById("app-style").getAttribute("href") != "/skote/assets/css/app.min.rtl.css")
-                        document.getElementById("app-style").setAttribute("href", "/skote/assets/css/app.min.rtl.css");
+                    if (document.getElementById("bootstrap-style").getAttribute("href") != window.ASSET_BASE_URL +"/skote/assets/css/bootstrap.min.rtl.css")
+                        document.getElementById("bootstrap-style").setAttribute("href", window.ASSET_BASE_URL +"/skote/assets/css/bootstrap.min.rtl.css");
+                    if (document.getElementById("app-style").getAttribute("href") != window.ASSET_BASE_URL +"/skote/assets/css/app.min.rtl.css")
+                        document.getElementById("app-style").setAttribute("href", window.ASSET_BASE_URL +"/skote/assets/css/app.min.rtl.css");
                     document.documentElement.setAttribute("dir", "rtl");
                     document.documentElement.setAttribute("data-bs-theme", "dark");
                     break;
