8be07d2496d16311ed5b21f0c1533ef6ea92a41a
helal <helal@logicsoftbd.com>
2025-08-03 22:53:04 +0600

    work order

--- resources/views/ajax/drop_down/location_under_company.blade.php
+++ resources/views/ajax/drop_down/location_under_company.blade.php
@@ -22,10 +22,23 @@
     }
     
     $lib_location = App\Models\LibLocation::where('company_id',$company_id)->pluck('location_name', 'id');
+
+    $selected = 0;
+    if(count($lib_location) == 1) {
+        $selected = $lib_location->keys()->first();
+    }
 ?>
 <select name="{{ $field_name }}" id="{{ $field_id }}"   class="form-control" onchange="<?php echo $on_change;?>">
     <option value="0">SELECT</option>
     @foreach($lib_location as $id => $location_name)
-        <option value="{{ $id }}">{{ $location_name }}</option>
+        <option value="{{ $id }}" {{ $selected == $id ? 'selected' : '' }}>{{ $location_name }}</option>
     @endforeach
 </select>
+<?php if($single_option && !empty($on_change)): ?>
+<script>
+    $(document).ready(function() {
+        // Execute the onchange JS if there’s only one option
+        <?php echo $on_change; ?> 
+    });
+</script>
+<?php endif; ?>
